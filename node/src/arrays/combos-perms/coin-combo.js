31ff695049659a2f2bb98532a24264b830b72753
Antonio Perez <aperez2541@gmail.com>
2020-04-15 15:30:55 -0700

    feat(coin-combo): add

---
+++ node/src/arrays/combos-perms/coin-combo.js
@@ -0,0 +1,62 @@
+// Coin Change Problem: Given a set of coin denominations, 
+// print out the different ways you can make a target amount. 
+// You can use as many coins of each denomination as you like. 
+// For example: If coins are [1,2,5] and the target is 5, 
+// output will be: [1,1,1,1,1][1,1,1,2][1,2,2][5]
+
+
+
+function printCoinChange(coins, target){
+  if(!coins || coins.length === 0 || target <= 0) {
+    return coins;
+  }
+
+  printCoinChangeHelper(coins, target, 0, [], 0);
+}
+
+function printCoinChangeHelper(coins, target, startIndex, buffer, sum){
+  if (sum > target){
+    return;
+  }
+
+  if(sum === target) {
+    console.log(buffer);
+    return;
+  }
+
+  for (let index = startIndex; index < coins.length; index++) {
+    buffer.push(coins[index]);
+    printCoinChangeHelper(coins, target, startIndex, buffer, sum + coins[index]);
+    buffer.pop()
+  }
+ }
+
+//printCoinChange([1,2,5], 5);
+
+function footballScoreCombos(scores, target) {
+  if(!scores || scores.length === 0 || target <= 0) {
+    return scores;
+  }
+
+  footballScoresHelper(scores, target, 0, [], 0);
+}
+
+function footballScoresHelper(scores, target, startIndex, buffer, sum) {
+  if (sum > target) {
+    return;
+  }
+
+  if(sum === target) {
+    console.log(buffer);
+    return;
+  }
+
+  for (let index = startIndex; index < scores.length; index++) {
+    const score = scores[index];
+    buffer.push(score);
+    footballScoresHelper(scores, target, index, buffer, sum + score);
+    buffer.pop();
+  }
+}
+
+footballScoreCombos([3,6,1], 10);
