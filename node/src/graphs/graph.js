d3ebc14f71c91aeb0116be00ecf6366671ee0ee3
Antonio Perez <aperez2541@gmail.com>
2020-04-23 00:46:28 -0700

    feat(graph): add search graph

---
+++ node/src/graphs/graph.js
@@ -0,0 +1,114 @@
+class Node {
+  constructor(data) {
+    this.neighbors = [];
+    this.data = data;
+    this.state = 'unvisited';
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  setData(data) {
+    this.data = data;
+  }
+
+  setState(state) {
+    this.state = state;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  addNeighbor(node) {
+    this.neighbors.push(node);
+  }
+
+  getNeighbors() {
+    return this.neighbors;
+  }
+}
+
+class Graph {
+  constructor(nodes) {
+    this.nodes = nodes;
+  }
+  
+  addNode(key, node) {
+    this.nodes[key] = node;
+  }
+
+  getNodes(){
+    return this.nodes;
+  }
+
+}
+
+// Given a graph and a target number T, find T exists in the graph.
+
+//Depth First Search
+function depthFirstSearch(graph, target) {
+  const nodes = graph.getNodes();
+  for (const node of nodes) {
+    if(node.getState() === 'unvisited' && depthFirstSearchVisit(node, target)){
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function depthFirstSearchVisit(node, target) {
+  node.setState('visiting');
+
+  if(node.getData() === target) {
+    return true;
+  }
+
+  const neighbors = node.getNeighbors();
+  for (const neighbor of neighbors) {
+    if(neighbor.getState() === 'unvisited' && depthFirstSearchVisit(neighbor, target)){
+      return true;
+    }
+  }
+
+  node.setState('visited');
+  return false;
+}
+
+//breadth first search
+function breadthFirstSearch(graph, target){
+  const nodes = graph.getNodes();
+  for (const node of nodes) {
+    if(node.getState() === 'unvisited' && breadthFirstSearchVisit(node, target)){
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function breadthFirstSearchVisit(startNode, target) {
+  const queue = [];
+  queue.enqueue(startNode);
+  startNode.setState('visiting');
+
+  while(queue.length > 0) {
+    const currentNode = queue.dequeue();
+    if(currentNode.getData() === target) {
+      return true;
+    }
+
+    for (const neighbor of currentNode.getNeighbors()) {
+      if(neighbor.getState() === 'unvisited') {
+        queue.enqueue(neighbor);
+        neighbor.setState('visiting');
+      }
+    }
+
+    currentNode.setState('visited');
+  }
+
+  return false;
+}
