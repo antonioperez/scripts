981088fe03b30dba2cc5a9e0443c303b7b797818
Antonio Perez <aperez2541@gmail.com>
2020-04-24 01:17:54 -0700

    re

--- node/src/graphs/topological-sort-longest-path.js
+++ node/src/graphs/topological-sort-longest-path.js
@@ -1,15 +1,14 @@
 // Diameter of a Graph: Given a directed graph, 
 // find the length of the longest path in the graph.
 
-getLongestPathCount(graph){
+getLongestPathCount(graph) {
   topoSortStack = topologicalSort(graph);
   let diameter = 0;
 
   while(topoSortStack.length > 0) {
     const currentNode = topoSortStack.pop();
     const currentLongPath = current.getLongestPath() + 1;
-
-    diameter = Math.max(diameter, current.getLongestPathCount());
+    diameter = Math.max(diameter, currentLongPath);
 
     for (const neighbor of currentNode.getNeighbors()) {
       if(currentLongPath > neighbor.getLongestPath()){
